2ec9802a82137962805a00c4e6a76c0fe82c9f9e
Marcelo Cesar Pereira Monteiro <marcelo.monteiro@SINESP.DF.GOV.BR>
2021-03-25 10:10:35 -0300

    CBR

--- src/components/Mapa.js
+++ src/components/Mapa.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
 import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-export default GoogleApiWrapper({
-  apiKey: (process.env.MAP_API)
-})(class Mapa extends Component {
+class Mapa extends Component {
 
-    constructor (props){
-        super(props)
-        this.state = {
-            selectedPlace : {
-                lat:0, lng: 0
-            },
-            selected : false
-        }
-      };
+  constructor (props){
+    super(props)
+    this.state = {
+        selectedPlace : {
+            lat:0, lng: 0
+        },
+        selected : false
 
-      componentDidMount () {
-        this.setState({selected : false})
-      }
+    };
+  }
+
+    componentDidMount () {
+      this.setState({selected : false})
+    }
 
       open(place, selected) {
         if(place.id !== 0 && selected){
@@ -25,19 +24,23 @@ export default GoogleApiWrapper({
         }
       }
 
-  render() {
-    return (
-        <Map google={this.props.google} zoom={14} initialCenter={{lat : this.props.place.latitude, lng : this.props.place.longitude}} >
-            <Marker
-            position={{
-                lat: this.props.place.latitude,
-                lng: this.props.place.longitude
-            }} onClick={() => {
-              this.setState({selected : true,
-                selectedPlace : this.props.place})
-            }}>
-            </Marker>
-        </Map>
-    );
-  }
-})
+      render() {
+        return (
+            <Map google={this.props.google} zoom={14} initialCenter={{lat : this.props.place.latitude, lng : this.props.place.longitude}} >
+                <Marker
+                position={{
+                    lat: this.props.place.latitude,
+                    lng: this.props.place.longitude
+                }} onClick={() => {
+                  this.setState({selected : true,
+                    selectedPlace : this.props.place})
+                }}>
+                </Marker>
+            </Map>
+        );
+      }
+      };
+      
+      export default GoogleApiWrapper({
+        apiKey: (process.env.MAP_API)
+      })(Mapa);
